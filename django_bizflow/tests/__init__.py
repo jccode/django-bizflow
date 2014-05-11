d33db1dcca45a2c8b80604c8b5a802b81ef7f1fa
jcchen <junchangchen@gmail.com>
2014-05-11 22:14:45 +0800

    init repo

---
+++ django_bizflow/tests/__init__.py
@@ -0,0 +1,3 @@
+
+from django_bizflow.tests.simpletest import *
+from django_bizflow.tests.models import * 
---
+++ django_bizflow/tests/simpletest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+
+class SimpleTest(TestCase):
+
+    def test_basic_addition(self):
+        self.assertEqual(1 + 1, 2)
+        
+        
+        
