f3388d0d03cc82bb3cbb66461889cc29c6b6cf32
jcchen <junchangchen@gmail.com>
2014-05-14 02:55:02 +0800

    update

--- example_app/admin.py
+++ example_app/admin.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
+from example_app.models import LeaveApplication
+
+
+class LeaveApplicationAdmin(admin.ModelAdmin):
+    exclude = ('is_agree', )
+    list_display = ('proposer', 'reason', 'start_time', 'end_time')
+
+
 
 # Register your models here.
+admin.site.register(LeaveApplication, LeaveApplicationAdmin)
--- example_app/models.py
+++ example_app/models.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
+from django.contrib.auth import models as auth
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 
@@ -6,11 +10,21 @@ class LeaveApplication(models.Model):
     """
     Leave application model
     """
+    proposer = models.ForeignKey(auth.User, related_name="+", verbose_name=_('Proposer'))
     reason = models.CharField(max_length=100, verbose_name=_('Reason'))
-    start_time = models.DateTimeField()
+    start_time = models.DateTimeField(verbose_name=_('Leave start time'))
+    end_time = models.DateTimeField(verbose_name=_('Leave end time'))
+    is_agree = models.BooleanField(default=False, verbose_name=_('Is agreed'))
 
+    class Meta:
+        verbose_name = _('Leave Application')
+        verbose_name_plural = _('Leave Applications')
+
+    def __unicode__(self):
+        return "%s - %s" % (self.proposer, self.reason)
         
         
+            
     
         
         
