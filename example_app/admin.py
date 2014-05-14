d8441e06f950d9d3231da4d364e4c6f06e591a75
jcchen <junchangchen@gmail.com>
2014-05-14 15:47:11 +0800

    crud

--- example_app/admin.py
+++ example_app/admin.py
@@ -5,10 +5,39 @@ from example_app.models import LeaveApplication
 
 
 class LeaveApplicationAdmin(admin.ModelAdmin):
-    exclude = ('is_agree', )
+    # exclude = ['is_agree', ]
+    # readonly_fields = ('proposer', )
     list_display = ('proposer', 'reason', 'start_time', 'end_time')
 
+    def get_queryset(self, request):
+        qs = super(LeaveApplicationAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(proposer=request.user)
 
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'proposer':
+            kwargs['initial'] = request.user.id
+        return super(LeaveApplicationAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = ['is_agree', ]
+        self.readonly_fields = ()
+        if not obj:             # add view
+            self.exclude.append('proposer')
+        else:
+            self.readonly_fields = ('proposer', )
+        return super(LeaveApplicationAdmin, self).get_form(request, obj, **kwargs)
+        
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.proposer = request.user
+        obj.save()
+        
+        
 
 # Register your models here.
 admin.site.register(LeaveApplication, LeaveApplicationAdmin)
